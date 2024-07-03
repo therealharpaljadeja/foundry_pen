894f1de7e135c0bc816b929a3cfcd6d071056603
evmBrahmin <evmbrahmin@gmail.com>
2024-07-03 17:53:58 -0400

    set trust proxy to heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// trust the first proxy in the chain which wil be Heroku's reverse proxy
+app.set('trust proxy', 1);
+
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
