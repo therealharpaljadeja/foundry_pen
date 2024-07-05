b3157b2a5016a465d3a127f2e979ec5783c25648
evmBrahmin <evmbrahmin@gmail.com>
2024-07-05 08:27:22 -0400

    update sessions to use cookies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['js-cookie']
+    }
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
