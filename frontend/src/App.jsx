a4144d63b74503b5a2650123eab707c8488cd6ee
evmBrahmin <evmbrahmin@gmail.com>
2024-07-08 08:23:44 -0400

    make reactive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,10 +61,10 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-[#23272F] text-white">
-      <div className="max-w-6xl mx-auto p-4 md:p-8">
+      <div className="max-w-full sm:max-w-6xl mx-auto p-2  sm:p-4 md:p-8">
         <div className="flex flex-col md:flex-row">
           <div className="flex-grow max-w-full md:max-w-4xl">
-            <h1 className="text-2xl md:text-3xl font-bold mb-6">Evaluating Contract Storage with Foundry</h1>
+            <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mb-6 break-words">Evaluating Contract Storage with Foundry</h1>
           
             {isLoading ? (
               <p className="text-[#61DAFB] text-sm mb-4">Loading session information...</p>
