333c1e22be85a05e4052edbe9696265f69dd430f
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-09-25 02:14:22 +0530

    fix: cors and cookies

--- src/index.js
+++ src/index.js
@@ -15,10 +15,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin:
-            process.env.FRONTEND_URL ||
-            "https://foundry-pen-86c9c65f23b0.herokuapp.com",
+        origin: process.env.FRONTEND_URL || "http://localhost:3000",
         credentials: true,
+        sameSite: "none",
     })
 );
 app.use(express.json());
